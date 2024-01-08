8631ea981a899a85552e0f763cded7d7ab99c1ba
NataliiaKulesh <ivankulesh0200@gmail.com>
2024-01-08 19:33:33 +0200

    Create Product.java

---
+++ src/main/java/org/example/Product.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Product {
+
+    public int id;
+    public String name;
+    public double price;
+
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+}
+
