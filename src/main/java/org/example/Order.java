8958aecbb480705a66c98ee9a8908588fe9d463a
NataliiaKulesh <ivankulesh0200@gmail.com>
2024-01-08 19:52:37 +0200

    Create Cart.java 2

--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -8,4 +8,12 @@ public class Order {
     public int orderId;
     public List<Product> products;
     public String status;
+
+//constr
+public Order(int orderId, List<Product> products, String status) {
+    this.orderId = orderId;
+    this.products = products;
+    this.status = status;
+}
+
 }
