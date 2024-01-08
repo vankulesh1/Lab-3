245aded5f515a03f9f788050b4e7d1303f8164d6
NataliiaKulesh <ivankulesh0200@gmail.com>
2024-01-08 19:41:14 +0200

    Create Cart.java 2

---
+++ src/main/java/org/example/Order.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.List;
+
+public class Order {
+
+    //Повинен мати атрибути, такі як `orderId`, `products`, `status`.
+    public int orderId;
+    public List<Product> products;
+    public String status;
+}
