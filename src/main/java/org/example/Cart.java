ff7ac3b444a9fef941314a250d1bde41561f57ce
NataliiaKulesh <ivankulesh0200@gmail.com>
2024-01-08 19:37:27 +0200

    Create Cart.java 2

--- src/main/java/org/example/Cart.java
+++ src/main/java/org/example/Cart.java
@@ -1,10 +1,14 @@
 package org.example;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Cart {
 
     private List<Product> products;
 
+    public Cart() {                          //constr
+        this.products = new ArrayList<>();
+    }
 
 }
