cb498f95b50c543424c974a8a5333a000c2e7f2a
NataliiaKulesh <ivankulesh0200@gmail.com>
2024-01-08 19:59:28 +0200

    Create Order.java 2

--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -1,19 +1,42 @@
 package org.example;
 
 import java.util.List;
+import java.util.ArrayList;
+
+
 
 public class Order {
 
     //Повинен мати атрибути, такі як `orderId`, `products`, `status`.
-    public int orderId;
-    public List<Product> products;
-    public String status;
+    private int orderId;
+    private List<Product> products;
+    private OrderStatus status;
 
 //constr
-public Order(int orderId, List<Product> products, String status) {
+public Order(int orderId, List<Product> products, OrderStatus status) {
     this.orderId = orderId;
     this.products = products;
     this.status = status;
 }
+//ввід / вивід
+public int getOrderId() {
+    return orderId;
+}
 
+    public List<Product> getProducts() {
+        return new ArrayList<>(products);
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+}
+//статуси продуктів
+enum OrderStatus {
+    ACCEPTED,
+    PROCESSING,
+    PACKED,
+    SENDING,
+    DELIVERY
 }
